ca35a45cb378f0254388cd383a50be2a11c22f36
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2024-06-30 12:48:40 +0330

    Implemented MySQLConnectionManager

--- Python/database/db_manager.py
+++ Python/database/db_manager.py
@@ -1,4 +1,3 @@
-from typing import Any
 import mysql.connector
 from mysql.connector import Error
 
@@ -9,3 +8,37 @@ class SingletonMeta(type):
         if cls._instance in None:
             cls._instance = super().__call__(*args, **kwargs)
         return cls._instance
+    
+class MySQLConnctionManager(metaclass=SingletonMeta):
+    def __init__(self, host, database, user, password) -> None:
+        self._connection = None
+        self._host = host
+        self._database = database
+        self._user = user
+        self._passwrod = password
+        self.connect()
+
+    def connect(self) -> None:
+        if self._connection is None:
+            try:
+                self._connection = mysql.connector.connect(
+                    host = self._host,
+                    database = self._database,
+                    user = self._user,
+                    password = self._passwrod
+                )
+                if self._connection.is_connected():
+                    print("Successfully connected to the database.")
+            except Error as e:
+                print(f"Error while connecting to server: {e}")
+                self._connection = None
+
+    def get_connection(self):
+        if not self._connection or not self._connection.is_connected():
+            self.connect()
+        return self._connection
+    
+    def close_conncetion(self):
+        if self._connection or self._connection.is_connected():
+            self._connection.close()
+            self._connection = None
