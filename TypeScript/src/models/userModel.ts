ef878979874418f5a214a42b64235f627362b4fc
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2024-07-03 14:16:20 +0330

    Added Table Creation logic

--- TypeScript/src/models/userModel.ts
+++ TypeScript/src/models/userModel.ts
@@ -3,6 +3,13 @@ import { Database } from "../database/db";
 export class UserModel {
     static async create(username:string, password:string, name:string): Promise<void> {
         const db = await Database.getInstance();
+
+        await db.execute(`CREATE TABLE IF NOT EXISTS users (
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            PRIMARY KEY (username)
+            );`);
         await db.execute("INSERT INTO users (username, password, name) VALUES (?, ?, ?)", [username, password, name]);
     }
 }
